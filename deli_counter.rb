727b12f681443fb0ea7eee750e05837b3c1d8ae3
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-22 19:32:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,13 @@
 katz_deli = []
 
-def line(array)
- if array.empty? 
-   puts "line is empty"
- 
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
 end
