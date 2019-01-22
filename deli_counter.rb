4fe161ca6223ab1c2a03e0a1c9c5d5fdbe4519d9
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-22 19:29:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,4 @@ katz_deli = []
 
 def line(array)
   array.empty?
-  if 
-    puts "line is currently empty"
- end 
-  else
-  puts "the line is currently serving: #{array.each do |name| puts "#{name}"
-
 end
