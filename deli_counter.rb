e9d8f6fd6609f5453b0233f0109c1aae521b10ad
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-22 19:29:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,10 @@ katz_deli = []
 
 def line(array)
   array.empty?
-  if puts "line is currently empty"
+  if 
+    puts "line is currently empty"
  end 
-  else puts "the line is currently serving: #{array.each do |name| puts "#{name}"
+  else
+  puts "the line is currently serving: #{array.each do |name| puts "#{name}"
 
 end
