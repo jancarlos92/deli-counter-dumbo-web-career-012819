f602a4e50f23f286a3e2a1ed7e3d2133a7489a6e
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-22 19:26:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line
   katz_deli.empty?
   if puts "line is currently empty"
   
-  elsif puts "the line is currently serving: #{katz_deli.each do |name| puts "#{name}"
+  else puts "the line is currently serving: #{katz_deli.each do |name| puts "#{name}"
 
 end
 end
